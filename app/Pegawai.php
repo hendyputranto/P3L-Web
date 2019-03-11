a47b6132fe92995b711b3da9722f71832a08f84e
sintaxd <jasintaxd@yahoo.com>
2019-03-11 14:31:12 +0700

    relasi one to many
    relasi diajarin nanda

--- app/Pegawai.php
+++ app/Pegawai.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Pegawai extends Model
 {
     //
+    protected $table = 'Pegawai';      //mendefine tabel yang digunakan
+    protected $primaryKey = 'id_pegawai';
+
+    public function role(){
+        return $this->belongsTo('app\Role');
+    }
 }
--- app/Role.php
+++ app/Role.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     //
+    protected $table = 'Role';      //mendefine tabel yang digunakan
+    protected $primaryKey = 'id_role';
+
+    public function pegawai(){
+        return $this->hasMany('app\Pegawai', 'id_role');
+    }
 }
