df7332c803f8a9a3370660686da9a563c74f11ca
sintaxd <jasintaxd@yahoo.com>
2019-03-11 15:37:42 +0700

    update
    update

--- app/Montir_OnDuty.php
+++ app/Montir_OnDuty.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Montir_OnDuty extends Model
 {
     //
+    protected $table = 'montir__on_duties';      //mendefine tabel yang digunakan
+    protected $primaryKey = 'id_montirOnDuty';
+    public function pegawai(){
+        return $this->belongsTo('app\Pegawai');
+    }
+
+    public function motor_konsumen(){
+        return $this->belongsTo('app\MotorKonsumen');
+    }
 }
